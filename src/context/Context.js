8195d58d91c46bc21ca743441ea1927be962b87a
mancia08 <teikomancini1993@gmail.com>
2020-12-16 21:16:54 +0100

    starting context

---
+++ src/context/Context.js
@@ -0,0 +1,24 @@
+import React, {Component} from "react"
+
+export const Context = React.createContext();
+
+class Provider extends Component {
+    state= {
+        example: "example"
+    }
+
+    test = () => { console.log(this.state.example)
+    this.setState({example: "hi"})
+console.log(this.state.example)
+}
+
+render () {
+    return <Context.Provider value={{
+        state: this.state,
+        test: this.test
+        /* if you write new functions write them here as i did with the test function */
+    }}>
+        {this.props.children}
+    </Context.Provider>
+}
+}
