4044273f63820cd94e8ac619d9d980f214923072
mancia08 <teikomancini1993@gmail.com>
2020-12-17 20:36:45 +0100

    merged everything

--- src/components/pageThree/PageThree.js
+++ src/components/pageThree/PageThree.js
@@ -1,49 +1,43 @@
 import React, { Component } from "react";
 import Navbar from "./../navbar/Navbar";
 import Footer from "./../footer/Footer";
-import "./PageThree.css"
+import "./PageThree.css";
 
-class PageThree extends Component{
+class PageThree extends Component {
   state = {
-    notEdible : [],
-  }
+    notEdible: [],
+  };
   // "https://trefle.io/api/v1/plants?token=lFM81UTKUliWbUM-9QDM0m3X8jPYbyFlAQrIQAFcRZA"
-componentDidMount(){
-  fetch("https://trefle.io/api/v1/plants?token=lFM81UTKUliWbUM-9QDM0m3X8jPYbyFlAQrIQAFcRZA&")
-  .then(res => res.json())
-  .then(data => 
-    this.setState({
-      notEdible: data.data
-    })
+  componentDidMount() {
+    fetch(
+      "https://trefle.io/api/v1/plants?token=lFM81UTKUliWbUM-9QDM0m3X8jPYbyFlAQrIQAFcRZA&"
     )
-}
+      .then((res) => res.json())
+      .then((data) =>
+        this.setState({
+          notEdible: data.data,
+        })
+      );
+  }
 
-render(){
-  return(
-    <div>
-      <Navbar />
-      <h1 className="titleNot">Not Edible</h1>
-      {
-      this.state.notEdible.map((names, index) =>
-      <div key={index}>
-        <h2 className="nameNot">{names.common_name}</h2>
-        <img className="imgNot" src={names.image_url} alt={names.scientific_name}></img>
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <h1 className="titleNot">Not Edible</h1>
+        {this.state.notEdible.map((names, index) => (
+          <div key={index}>
+            <h2 className="nameNot">{names.common_name}</h2>
+            <img
+              className="imgNot"
+              src={names.image_url}
+              alt={names.scientific_name}
+            ></img>
+          </div>
+        ))}
+        <Footer />
       </div>
-      )}
-      <Footer />
-    </div>
-  )
+    );
+  }
 }
-
-}
-
-
-
-
-
-
-
-
-
-
-export default PageThree
+ export default PageThree
