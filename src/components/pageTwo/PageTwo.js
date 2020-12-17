001a6a5994189859e0c0a7afcb5b98b23a0d5d14
Alfred <castellum.a@gmail.com>
2020-12-17 22:21:42 +0100

    update edible plants css

--- src/components/pageTwo/PageTwo.js
+++ src/components/pageTwo/PageTwo.js
@@ -1,12 +1,38 @@
+import React, { Component } from 'react';
 import Navbar from "./../navbar/Navbar";
 import Footer from "./../footer/Footer";
-
-const PageTwo = () => (
-  <>
-    <Navbar />
-    <>MY CONTENT</>
-    <Footer />
-  </>
-);
-
+import "./PageTwo.css"
+const ediblePlants = 'https://trefle.io/api/v1/plants?&token=lFM81UTKUliWbUM-9QDM0m3X8jPYbyFlAQrIQAFcRZA&filter[edible]=true';
+const apiCaller = url => fetch(url).then(result => result.json())
+class PageTwo extends Component {
+  state = {
+    ediblePlant: []
+  }
+  componentDidMount() {
+    apiCaller(ediblePlants)
+      .then(data =>
+        this.setState({
+          ediblePlant: data.data
+        })
+      )
+  }
+  render() {
+    return (
+      <div><Navbar />
+        <h1>Edible Plants</h1>
+        <div className='flexContainer'>
+          {
+            this.state.ediblePlant.map((iterator, index) =>
+              <div className='plantContainer' key={index}>
+                <h2>{iterator.common_name}</h2>
+                  <img id='pageOneImage' src={iterator.image_url} alt={iterator.scientific_name}></img>
+              </div>
+            )
+          }
+        </div>
+        <Footer />
+      </div>
+    )
+  }
+}
 export default PageTwo;
