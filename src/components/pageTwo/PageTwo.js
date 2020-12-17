64aeb5049ca740c498d6e0c97760dbbb44060899
Cayuela Jean-Loup <jeanloupcayuela30400@gmail.com>
2020-12-17 17:54:25 +0000

    update

--- src/components/pageTwo/PageTwo.js
+++ src/components/pageTwo/PageTwo.js
@@ -1,19 +1,5 @@
 import Navbar from "./../navbar/Navbar";
 import Footer from "./../footer/Footer";
-<<<<<<< HEAD
-
-
-/* WHEN YOU NEED DATA OF API CALL FOR api.something WHEN YOU NEED DATA FROM CONTEXT CALL value.something */
-
-const PageTwo = () => (
- 
-          <>
-            <Navbar />
-            <>MY CONTENT</>
-            <Footer />
-          </>
-       
-=======
 
 const PageTwo = () => (
   <>
@@ -21,7 +7,6 @@ const PageTwo = () => (
     <>MY CONTENT</>
     <Footer />
   </>
->>>>>>> 9089aa2bd32f8b225105b4db08d57f039650e222
 );
 
 export default PageTwo;
